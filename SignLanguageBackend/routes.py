0ac59a9835f6600b16be8e243f63e7c2cf0531a9
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2020-06-20 14:50:42 +0200

    Post Buffering

--- SignLanguageBackend/routes.py
+++ SignLanguageBackend/routes.py
@@ -13,6 +13,7 @@ model_map = {
 @app.route('/api/cnn_5_150_150', methods=["GET", "POST"])
 def cnn_5_150_150():
     try:
+        print(request.form)
         print(request.data)
         print(request.args)
         print(request.files)

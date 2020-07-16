c2408a8b6ef116540377edf6fa7af20b22daf281
Sebastian Schmidt <sebastian@jupiter.fh-swf.de>
2020-07-16 17:27:15 +0200

    More Models + Requirements + Readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SignLanguageBackend
+Dieses Backend gibt Zugriff auf verschiedene CNN Modelle zur Klassifikation
+von Gebärdensprache. Die Modelle werden dabei entweder im FileStorage oder 
+Base64 kodierter String in den Postdaten eines Requests übermittelt. Danach
+wird es auf verschiedene Arten vorverarbeitet und klassifiziert. Weiterhin gibt 
+das Backend Informationen zu verfügbaren Modellen und ihren URLs.
+## Installation
+Das Backend wurde unter Debian 10 und Linux Mint 19 getestet sowie entwickelt. 
+Um möglichen Inkompatibilitäten aus dem Weg zu gehen, werden diese als Betriebssystem
+empohlen.
+1. Installation von essentiellen Paketen mit: apt-get install build-essential python
+2. Installation von Conda oder einem anderen Python Umgebungsmanager.
+3. Erstellen einer neuen Python 3.8 Umgebung.
+4. Installation der Anforderungen aus der requirements.txt mit PIP.
+5. Starten des UWSGI-Servers aus dem Hauptverzeichnis der Anwendung mit: uwsgi --ini sign_language.ini
+
+Aus diesem Befehl resultiert eine sign_language.sock Datei, auf welche beispielsweise mit NGINX Zugriff 
+gewährt werden kann. Wahlweise lässt sich die API auch unter https://jupiter.fh-swf.de/sign-language/ aufrufen, 
+wo die neueste Version gehostet ist.
--- SignLanguageBackend/services.py
+++ SignLanguageBackend/services.py
@@ -5,31 +5,47 @@ from SignLanguageBackend.ModelInfo import ModelInfo
 
 # Gültige Urls für Modelle
 model_urls = {
-    "150x150_5_Layer_CNN.hdf5": "/cnn_5_150_150",
-    "224x224_VGG19_CNN.hdf5": "/vgg19_224_224",
-    "224x224_VGG19_CNN_v2.hdf5": "/vgg19_224_224_v2"
+    "150x150_5_Layer_CNN.hdf5": "/150x150_cnn_5",
+    "224x224_VGG19_CNN.hdf5": "/224x224_vgg19",
+    "224x224_VGG19_CNN_v2.hdf5": "/224x224_vgg19_v2",
+    "VGG19_Kaggle2.hdf5": "/224x224_vgg19_v3",
+    "CNN_Kaggle2.hdf5": "/150x150_cnn_5_v2"
 }
 
 # Infos der Modelle mit URLs zurückgeben
 api.add_resource(ModelInfo, "/model_info", endpoint="model_info", resource_class_kwargs={"model_urls": model_urls})
 
 # Eigenes Modell mit Eingangsgröße 150x150 und 5 CNN Layern.
-api.add_resource(SignLanguageCNN, model_urls["150x150_5_Layer_CNN.hdf5"], endpoint="cnn_5_150_150",
+api.add_resource(SignLanguageCNN, model_urls["150x150_5_Layer_CNN.hdf5"], endpoint="150x150_cnn_5",
                  resource_class_kwargs={"document_outputs": False,
                                         "target_shape": (150, 150),
                                         "model_config": app.config["MODELS"]["150x150_5_Layer_CNN.hdf5"]})
 
 # VGG19 mit Eingangsgröße 224x224 umtrainiert.
-api.add_resource(SignLanguageCNN, model_urls["224x224_VGG19_CNN.hdf5"], endpoint="vgg19_224_224",
+api.add_resource(SignLanguageCNN, model_urls["224x224_VGG19_CNN.hdf5"], endpoint="224x224_vgg19",
                  resource_class_kwargs={"document_outputs": False,
                                         "to_grayscale": False,
                                         "target_shape": (224, 224),
                                         "model_config": app.config["MODELS"]["224x224_VGG19_CNN.hdf5"]})
 
 # VGG19 mit Eingangsgröße 224x224 umtrainiert und mit VGG19_Preprozess der Bilder.
-api.add_resource(SignLanguageCNN, model_urls["224x224_VGG19_CNN_v2.hdf5"], "/default", endpoint="vgg19_224_224_v2",
+api.add_resource(SignLanguageCNN, model_urls["224x224_VGG19_CNN_v2.hdf5"], endpoint="224x224_vgg19_v2",
                  resource_class_kwargs={"document_outputs": False,
                                         "to_grayscale": False,
                                         "target_shape": (224, 224),
                                         "model_config": app.config["MODELS"]["224x224_VGG19_CNN_v2.hdf5"]})
 
+# VGG19 mit Eingangsgröße 224x224 umtrainiert, mit VGG19_Preprozess der Bilder und
+# deaktivierten Basemodeltraining. Bekannt aus Ausarbeitung.
+api.add_resource(SignLanguageCNN, model_urls["VGG19_Kaggle2.hdf5"], "/default", endpoint="224x224_vgg19_v3",
+                 resource_class_kwargs={"document_outputs": False,
+                                        "to_grayscale": False,
+                                        "target_shape": (224, 224),
+                                        "model_config": app.config["MODELS"]["VGG19_Kaggle2.hdf5"]})
+
+# CNN bekannt aus Ausarbeitung für den ASL-Alphabet Datensatz.
+api.add_resource(SignLanguageCNN, model_urls["CNN_Kaggle2.hdf5"], endpoint="150x150_cnn_5_v2",
+                 resource_class_kwargs={"document_outputs": False,
+                                        "to_grayscale": True,
+                                        "target_shape": (200, 200),
+                                        "model_config": app.config["MODELS"]["CNN_Kaggle2.hdf5"]})
